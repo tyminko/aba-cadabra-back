d6a830c68ebd5f8baa8b3c5dcb7ed01b850741fc
maxim <tyminko@gmail.com>
2020-02-17 02:33:33 +0100

    add config

---
+++ functions/src/config.ts
@@ -0,0 +1,9 @@
+// rootEmail: a user with this email will automatically (on creation) receive an Admin role.
+// IMPORTANT! If not provided, then you won't be able to set user roles at all.
+export const rootEmail = '...'
+
+// withUserSearchIndices if true, then for each user name a 'pyramid' of search strings will be generated. This is in order to allow a pseudo-"full-text" search for user profiles.
+export const withUserSearchIndices = true
+
+// withTagSearchIndices if true, then for each tag title a 'pyramid' of search strings will be generated. This is in order to allow a pseudo-"full-text" search for tags.
+export const withTagSearchIndices = true
